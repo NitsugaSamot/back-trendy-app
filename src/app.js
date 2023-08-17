6c4efe2de53559de98a2c4d32c1d93c501561e4a
tomascallenius <tomascallenius96@gmail.com>
2023-08-17 19:38:12 -0300

    si no anda yo no fui XD2

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    `${VERCEL_FRONTEND_URL} || ${VITE_FRONTEND_URL}`
+    `${VERCEL_FRONTEND_URL}`
   ); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');

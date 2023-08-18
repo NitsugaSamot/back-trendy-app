7967c9e7a734bf97cebf76df49c908b91a2cf6a8
tomascallenius <tomascallenius96@gmail.com>
2023-08-18 12:18:53 -0300

    nodemailer7

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -9,7 +9,6 @@ const emailRegister = async (data) => {
   const transport = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -49,7 +48,6 @@ const emailForgetPassword = async (data) => {
   });
 
   //Informacion del email
-//dsfffffffffffffffffffffffffffffffffffffffffffffff
   // A continuación, se define un objeto info que contiene la información del correo electrónico a enviar. El correo electrónico incluye un remitente, destinatario, asunto, texto plano y contenido HTML. Se utiliza el enlace ${process.env.FRONTEND_URL}/confirmar/${token} para generar un enlace de confirmación de cuenta en el contenido HTML del correo electrónico. El token se inserta en el enlace y se enviará al usuario para que pueda confirmar su cuenta haciendo clic en él.
 
   const info = await transport.sendMail({

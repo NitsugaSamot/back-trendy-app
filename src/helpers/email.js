c149809bb84e20e94cd6ed3e77f9895cbf0266f8
tomascallenius <tomascallenius96@gmail.com>
2023-08-18 11:21:23 -0300

    nodemailer3

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -9,7 +9,7 @@ const emailRegister = async (data) => {
   const transport = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,

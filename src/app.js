ed148752fd6e688a7e2b0ec8c80f69fdd0109f11
tomascallenius <tomascallenius96@gmail.com>
2023-08-19 23:57:43 -0300

    back

--- src/app.js
+++ src/app.js
@@ -28,7 +28,8 @@ server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header(
-    "Access-Control-Allow-Origin", "*"
+    "Access-Control-Allow-Origin",
+    "https://front-trendy-app.vercel.app"
   ); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(

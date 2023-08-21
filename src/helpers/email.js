bc89f99409cf89306ce742aa0d091facc3af4dc7
F.Fernandez <hellsingsd@gmail.com>
2023-08-21 19:01:06 -0300

    isDelete

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -47,7 +47,7 @@ const emailForgetPassword = async (data) => {
       pass: process.env.EMAIL_PASS,
     },
   });
-
+ 
   //Informacion del email
   // A continuación, se define un objeto info que contiene la información del correo electrónico a enviar. El correo electrónico incluye un remitente, destinatario, asunto, texto plano y contenido HTML. Se utiliza el enlace ${process.env.FRONTEND_URL}/confirmar/${token} para generar un enlace de confirmación de cuenta en el contenido HTML del correo electrónico. El token se inserta en el enlace y se enviará al usuario para que pueda confirmar su cuenta haciendo clic en él.
 

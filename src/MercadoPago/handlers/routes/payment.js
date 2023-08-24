c26fc9e41453eede63b338eb1822c6e7a6f72b46
LucasGonzalezRighi <lucasgonzalezrighi@gmail.com>
2023-08-24 03:12:34 -0300

    didi

--- src/MercadoPago/handlers/routes/payment.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const successfulPayment = require("../success");
-const placeOrder = require("../placeOrder")
-
-const paymentRouter = Router();
-paymentRouter.post("/order", placeOrder);
-paymentRouter.get("/success", successfulPayment);
-
-module.exports = paymentRouter;
